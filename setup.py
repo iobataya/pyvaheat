2f8024aad1c475aaed4a20169c71c28772d22e20
Ikuo Obataya <i.obataya@gmail.com>
2024-02-04 07:06:35 +0900

    Added __init__.py and setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name='pyvaheat',
+    version='0.1.0',
+    description='Wapper class for VAHEAT serial controller API',
+    author='Ikuo Obataya',
+    author_email='obataya@qd-japan.com',
+    packages=find_packages(),
+)
