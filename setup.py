7d88cb363e310dd9740b63153fa92c7459e391c2
Ikuo Obataya <i.obataya@gmail.com>
2024-02-08 16:17:30 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,10 @@ setup(
     description='Wapper class for VAHEAT serial controller API',
     author='Ikuo Obataya',
     author_email='obataya@qd-japan.com',
+    url='https://github.com/iobataya/pyvaheat.git',
     packages=find_packages(),
+    long_description='This class and CLI can controll VAHEAT controller on python.',
+    install_requires=[
+        'pyserial',
+    ]
 )
